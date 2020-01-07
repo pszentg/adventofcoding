9b6df9e67996f5be27b1d1d6aa5b85fe473418f3
Pal Szentgyorgyi <lyerit+github@gmail.com>
2020-01-07 18:30:10 +0100

    PEP8
    complying with PEP8 line lengths

--- 2019/extract_tasks.py
+++ 2019/extract_tasks.py
@@ -12,9 +12,15 @@ def remove_tags(s):
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
 # only extracts the tasks, the resources has to be downloaded after auth
-# the author asked not to automate stuff too much, therefore I downloaded those manually
+# the author asked not to automate stuff too much, therefore I downloaded
+# those manually
+
+# also mind you, some of the tasks had multiple parts which gets unlocked if
+#  you submit a correct answer to the first. this script only pulls the
+# first parts of the tasks
 for i in range(1, 25):
-    with urllib.request.urlopen(f'https://adventofcode.com/2019/day/{i}', ) as response:
+    with urllib.request.urlopen(
+            f'https://adventofcode.com/2019/day/{i}', ) as response:
         if response.code == 200:
             html = response.read()
             soup = BeautifulSoup(html, features='html.parser')
